c355d51b3d986dc7127a899532d9751a7df7d075
Lobo Chu <Lobo.Chu@audatex.com>
2019-09-24 17:53:02 +0800

    Add upload part

--- ocr-service/project.clj
+++ ocr-service/project.clj
@@ -1,15 +1,17 @@
 (defproject ocr-service "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple OCR service"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [net.sourceforge.tess4j/tess4j "4.4.0"]]
+                 [net.sourceforge.tess4j/tess4j "4.4.0"]
+                 [org.clojure/tools.namespace "0.3.1"]
+                 [http-kit "2.3.0"]]
   
   :plugins [[lein-ring "0.12.5"]
             [lein-ancient "0.6.15"]]
-  :main ocr-service.ocr
+  :main ocr-service.handler
   :ring {:handler ocr-service.handler/app
          :nrepl { :start true  :port 6000}}
   :profiles
--- ocr-service/src/ocr_service/handler.clj
+++ ocr-service/src/ocr_service/handler.clj
@@ -6,8 +6,29 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (route/not-found "Not Found")
-  (POST "/ocr" [file] (ocr/scan-ocr-file file)))
+  
+  
+  (POST "/ocr" request
+    (let [file (-> request :file)
+          content (-> request :content)]
+      do(
+         (println (str "file:-------->>>>>>>>>>" file  "content: " content))
+         (ocr/scan-ocr-file content))))
+  (POST "/ocr1"
+    {form-params :form-params} "ocr1")
+  (POST "/ocr2" request
+    (str (:form-params request)))
+  ;this line must be last !!
+  (POST "/ocr3" 
+    {{ {tempfile  :tempfile filename :filename} :file} :params :as params}
+    (println (str "filename " filename " tempfile: " tempfile " params: " params))
+    (ocr/scan-ocr-file tempfile))
+  (route/not-found "Not Found"))
+
+          
+      
+    
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes 
+    (assoc-in site-defaults [:security :anti-forgery] false)))
